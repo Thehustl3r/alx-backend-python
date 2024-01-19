9470b7282e7d5a41d6b1cfa9bf0e1ca6ecfdfc36
Prosper Mugisha <mprosp3r@gmail.com>
2024-01-19 12:54:40 +0200

    fix annotation problem

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay:  int = 10) -> float:
     """the function that generate a random number between 0 and max"""
 
     random_num = random.uniform(0, max_delay)

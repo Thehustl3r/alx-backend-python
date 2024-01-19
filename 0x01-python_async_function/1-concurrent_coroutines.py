222dc116016fe6bfaf1cbe4e3de80f9b8d8bbbbd
Prosper Mugisha <mprosp3r@gmail.com>
2024-01-19 12:59:01 +0200

    fix annotation problem

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,12 +2,12 @@
 """
 1-concurent_coroutines.py
 """
-import random
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """the function that generate a random number and save them in the list"""
 
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]

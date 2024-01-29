8d69ec2c1c481d154f2444b019cc286060411ae4
Prosper Mugisha <mprosp3r@gmail.com>
2024-01-29 03:14:34 +0200

    my first commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """./0-async_generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator:
     """async function that will wait random time"""
     for _ in range(10):
         await asyncio.sleep(1)

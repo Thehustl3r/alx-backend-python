91d96dfd07ae1cbf36068cbeb9c92c5de29d54cb
Prosper Mugisha <mprosp3r@gmail.com>
2024-01-29 02:37:57 +0200

    annotation of advanced tasks

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+use of type script in python
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+use of type script in python
+"""
+from typing import Sequence, Any, Union, Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    # return tuple(zoomed_in)
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(tuple(array))
+
+zoom_3x = zoom_array(tuple(array), 3)

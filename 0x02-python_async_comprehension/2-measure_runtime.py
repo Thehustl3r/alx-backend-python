80516bcbd1968584d0c7bc451be42e4603960af6
Prosper Mugisha <mprosp3r@gmail.com>
2024-01-29 03:41:23 +0200

    2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""./"""
+import asyncio
+import random
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """async function that will return the array """
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension())
+    end_time = time.perf_counter()
+    return end_time - start_time
